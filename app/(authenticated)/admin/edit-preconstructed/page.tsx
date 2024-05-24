c09e36b319361dcd958c506d9f1f9f37913ecaeb
rohit-simbanic <rohit.simbanic2023@gmail.com>
2024-05-24 21:33:17 +0530

    fixed

--- app/(authenticated)/admin/edit-preconstructed/page.tsx
+++ app/(authenticated)/admin/edit-preconstructed/page.tsx
@@ -1,12 +1,7 @@
 import EditPreConstructedPropertyForm from "@/theme/components/form/edit-preconstructed";
-import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      <EditPreConstructedPropertyForm />
-    </div>
-  );
+  return <EditPreConstructedPropertyForm />;
 };
 
 export default page;
--- app/(authenticated)/admin/edit-property/page.tsx
+++ app/(authenticated)/admin/edit-property/page.tsx
@@ -1,12 +1,6 @@
 import EditPropertyForm from "@/theme/components/form/edit-property";
-import React from "react";
-
 const page = () => {
-  return (
-    <div>
-      <EditPropertyForm />
-    </div>
-  );
+  return <EditPropertyForm />;
 };
 
 export default page;
