8497d40fd05fae0536eac9f182ec1e00c8d0230b
rohit-simbanic <rohit.simbanic2023@gmail.com>
2024-06-06 21:09:43 +0530

    6june

--- app/(defaults)/page.tsx
+++ app/(defaults)/page.tsx
@@ -1,11 +1,8 @@
+"use client";
 import { useAuth } from "@/contexts/auth-provider";
 import HomePage from "@/pages/home/home";
 import { Metadata } from "next";
 import React from "react";
-
-export const metadata: Metadata = {
-  title: "Home Page",
-};
 
 const Home = () => {
   const { isAuthenticated } = useAuth();
