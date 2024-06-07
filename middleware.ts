c19f242a12ef85a2210b00f2ce03119221373c6b
rohit-simbanic <rohit.simbanic2023@gmail.com>
2024-06-07 20:21:35 +0530

    7june

---
+++ middleware.ts
@@ -0,0 +1,44 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // Extract pathname from the request URL
+  const { pathname } = request.nextUrl;
+
+  // List of paths that are allowed, feel free to customize this as needed
+  const allowedPaths = [
+    "/",
+    "/admin",
+    "/admin/agent-profile",
+    "/admin/login",
+    "/admin/signup",
+    "/admin/signup/reset-password",
+    "/contact",
+    "/land-transfer",
+    "/mortgage-amortization",
+    "/mortgage-payment",
+  ];
+  const dynamicPathsPatterns = [
+    /^\/[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/, // Matches paths like /123
+    /^\/admin\/signup\/reset-password-client\/[a-zA-Z0-9_-]+$/, // Matches /admin/signup/reset-password-client/abc-123
+  ];
+
+  const isAllowed =
+    allowedPaths.includes(pathname) ||
+    dynamicPathsPatterns.some((pattern) => pattern.test(pathname));
+
+  if (!isAllowed) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // Continue with the request if it's an allowed path
+  return NextResponse.next();
+}
+
+// Specify the paths where the middleware should be applied
+export const config = {
+  matcher: [
+    "/((?!api|_next|favicon.ico).*)", // Match all paths except for API routes, static files, and Next.js internals
+  ],
+};
